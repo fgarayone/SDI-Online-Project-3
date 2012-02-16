3dbcde200c503ad4ed6eeaf877b361dc618eebac
Freddie Garay <fgarayone@fullsail.edu>
2012-02-16 16:07:16 -0500

    Begin JavaScript

--- sdi-project3/garay_freddie_project3.js
+++ sdi-project3/garay_freddie_project3.js
@@ -1,1 +1,12 @@
-alert("JavaScript works!");
+//FREDDIE GARAY
+// SDI ONLINE PROJECT 3
+// TERMNUMBER 0212
+
+// Story Begins
+console.log ( "On the way to the city of Omagua, we had to stop for a rest. The city was full of refugee's that were affected ")
+console.log ( "by the Anti-Benders. The Anti-benders were group dedicated only to suppressing the freedom of having bending ability")
+console.log ( "We had to fight and defeat the anti-benders to leave the city. Each single one of us took turns fighting and freeing");
+console.log ( "the city from these ruthless people."
+
+// JavaScript Starts
+
